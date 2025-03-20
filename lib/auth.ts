f8b47c69a54e95f3f2737579b88e131d587ee5e3
HimeshPremji <himeshdmaheswary@gmail.com>
2025-03-20 12:22:59 +0500

    Implement authentication logic with NextAuth, including user validation and session management

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { connectToDB } from "./db";
 import User from "../src/models/User";
+import bcrypt from "bcryptjs";
 
 export const authOptions: NextAuthOptions = {
   providers: [
@@ -21,9 +22,49 @@ export const authOptions: NextAuthOptions = {
         try {
           await connectToDB();
 
-          const newUser = await User.findOne({ email: credentials.email });
-        } catch (error) {}
+          const user = await User.findOne({ email: credentials.email });
+          if (!user) {
+            throw new Error("No user found");
+          }
+
+          const isValid = await bcrypt.compare(
+            credentials.password,
+            user.password
+          );
+          if (!isValid) {
+            throw new Error("Password doesn't match");
+          }
+
+          return {
+            id: user._id.toString(),
+            email: user.email,
+          };
+        } catch (error) {
+          throw new Error(`Error while logging in via credentials ${error}`);
+        }
       },
     }),
   ],
+
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.id = user.id;
+      return token;
+    },
+    async session({ session, token }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
+
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  secret: process.env.NEXT_AUTH_SECRET,
 };
